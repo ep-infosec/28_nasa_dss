afa6dc61b5308403677067b3cf3531d29dcc23bd
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-10 10:08:01 +0200

    init

---
+++ build/dev/startup/core_service.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# This startup script is meant to be invoked from within a Docker container
+# started by docker-compose_dss.yaml, not on a local system.
+
+/startup/wait_for_bootstrapping.sh
+
+DEBUG_ON=${1:-0}
+
+if [ "$DEBUG_ON" = "1" ]; then
+  echo "Debug Mode: on"
+
+  dlv --headless --listen=:4000 --api-version=2 --accept-multiclient exec --continue /usr/bin/core-service -- \
+  -cockroach_host local-dss-crdb \
+  -public_key_files /var/test-certs/auth2.pem \
+  -log_format console \
+  -dump_requests \
+  -addr :8082 \
+  -accepted_jwt_audiences localhost,host.docker.internal,local-dss-core-service,dss_sandbox_local-dss-core-service_1,core-service \
+  -enable_scd \
+  -enable_http
+else
+  echo "Debug Mode: off"
+
+  /usr/bin/core-service \
+  -cockroach_host local-dss-crdb \
+  -public_key_files /var/test-certs/auth2.pem \
+  -log_format console \
+  -dump_requests \
+  -addr :8082 \
+  -accepted_jwt_audiences localhost,host.docker.internal,local-dss-core-service,dss_sandbox_local-dss-core-service_1,core-service \
+  -enable_scd \
+  -enable_http
+fi
+
